f78f3dc3f10d2c4fa1cfbe422b3ac7342fa80225
Sauzan Faza <sauzanfaza@gmail.com>
2025-08-09 14:02:58 +0700

    membuat style navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return(
+        <>
+        <header className="w-full p-4 flex items-center justify-between bm-5 bg-[#5C3D2E] text-[#FEFED2]">
+            <div className="flex items-center pl-4 font-bold mr-5 text-lg">SourDough.</div>
+            <div className="flex items-center justify-between gap-6 font-josefin">
+                <a href="" className="">menu</a>
+                <a href="" className="">contact</a>
+                <a href="">package</a>
+            </div>
+        </header>
+        </>
+    )
+}
