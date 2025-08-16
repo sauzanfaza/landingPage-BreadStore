6de3c0b33bf2caebcdba4aa5d5f782577e7737c7
Sauzan Faza <sauzanfaza@gmail.com>
2025-08-16 20:35:47 +0700

    adding animation in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,23 @@
+import { motion } from "motion/react"
+
 export default function About() {
     return(
         <div className="flex flex-col md:flex-row items-center bg-yellow-50 min-h-screen py-10 px-5">
-        <div className="w-full md:w-1/2 p-6 text-center md:text-left">
+        <div className="w-full p-4 text-center md:text-left">
             <h2 className="text-2xl font-bold mb-4 font-josefin">Judul Sourdough</h2>
             <p className="text-gray-800 font-josefin">
             this is about sourdough bread website, soon i will write some paragraph here...
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolorem aspernatur iure rem accusamus commodi at porro omnis. Inventore tempore iusto molestias aperiam exercitationem vitae recusandae nulla quisquam ut. Minus?
             </p>
         </div>
-        <div className="flex flex-row items-center w-1/2">
-            <img src="/gallery/loaf1.jpeg" alt="" className="mix-blend-multiply" />
+        <div className="flex mx-4"></div>
+        <div className="flex flex-row items-center max-w-sm justify-center">
+            <motion.img 
+            className="mix-blend-multiply"
+            src="/gallery/loaf1.jpeg" 
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            />
         </div>
         </div>
 
